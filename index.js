8b917c23f64291042b0e67421134f197b312874b
Carlos Cuamatzin <kuamatzin@gmail.com>
2016-12-21 10:16:04 -0600

    Web

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ var server = require('http').Server(app);
 
 var io = require('socket.io')(server);
 
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+app.get('/', function(request, response){
+    response.send("HOLA MUNDO");
+});
+
 io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('new', function(msg) {
         console.log(msg.data);
         io.emit('saludo', { data: msg.data });
     });
-});
-
-server.listen(3000, function(){
-  console.log('listening on *:3000');
 });
